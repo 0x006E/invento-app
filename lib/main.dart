94982655fd71523c7b44c9b7e7a5bcb7edbf6759
Nithin S Varrier <nithin486@hotmail.com>
2023-03-18 09:39:42 +0530

    Setup Dynamic Colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:invento/screens/homepage.dart';
 
@@ -7,19 +8,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static final _defaultLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.blue);
+
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
+      primarySwatch: Colors.blue, brightness: Brightness.dark);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Invento',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink),
-        appBarTheme: const AppBarTheme(elevation: 0),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
+    return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      return MaterialApp(
+        title: 'Invento',
+        theme: ThemeData(
+          colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData(
+          colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+          useMaterial3: true,
+        ),
+        themeMode: ThemeMode.dark,
+        home: const HomePage(),
+      );
+    });
   }
 }
